e3fee91b08be7120372fb345393cce0b38ba5c6f
himel8 <himelkhan290@gmail.com>
2025-02-01 01:52:03 +0600

    added the caption text on the video

--- app/dashboard/_component/RemotionVideo.jsx
+++ app/dashboard/_component/RemotionVideo.jsx
@@ -1,4 +1,11 @@
-import { AbsoluteFill, Audio, Img, Sequence, useVideoConfig } from "remotion";
+import {
+  AbsoluteFill,
+  Audio,
+  Img,
+  Sequence,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
 
 const RemotionVideo = ({
   script,
@@ -7,12 +14,21 @@ const RemotionVideo = ({
   setDurationInFrame,
 }) => {
   const { fps } = useVideoConfig();
+  const frame = useCurrentFrame();
 
   const getDurationFrams = () => {
     setDurationInFrame((captions[captions?.length - 1]?.end / 1000) * fps);
     return (captions[captions?.length - 1]?.end / 1000) * fps;
   };
-  console.log(audioFileUrl);
+
+  const getCurrentCaptions = () => {
+    const currentTime = (frame / 30) * 1000;
+    const currentCaption = captions.find(
+      (word) => currentTime >= word.start && currentTime <= word.end
+    );
+    return currentCaption ? currentCaption?.text : "";
+  };
+
   return (
     <AbsoluteFill className="bg-black">
       {script?.map((item, index) => (
@@ -22,14 +38,33 @@ const RemotionVideo = ({
             from={(index * getDurationFrams()) / script?.length}
             durationInFrames={getDurationFrams()}
           >
-            <Img
-              src={`/screens/${item.scene}.jpg`}
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
+            <AbsoluteFill
+              style={{ justifyContent: "center", alignItems: "center" }}
+            >
+              <Img
+                src={`/screens/${item.scene}.jpg`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+              <AbsoluteFill
+                style={{
+                  color: "white",
+                  justifyContent: "center",
+                  top: undefined,
+                  bottom: 50,
+                  height: 150,
+                  textAlign: "center",
+                  width: "100%",
+                }}
+              >
+                <h2 className="text-2xl bg-[#00000066]">
+                  {getCurrentCaptions()}
+                </h2>
+              </AbsoluteFill>
+            </AbsoluteFill>
           </Sequence>
         </>
       ))}
